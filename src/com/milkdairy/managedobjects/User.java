200a73284dee4accade345e8fab16684446735c5
nagarjuna <nagarjuna.g@techouts.com>
2016-09-09 13:26:18 +0530

    user commite

--- src/com/milkdairy/managedobjects/User.java
+++ src/com/milkdairy/managedobjects/User.java
@@ -2,13 +2,76 @@ package com.milkdairy.managedobjects;
 
 public class User {
 
-	private String name;
+	private String fName;
+	private String lName;
 	private String userName;
 	private String password;
 	private String fevPetName;
 	private String phoneNum;
 	private String imageUrl;
 	private int logingStatus;
-	private String address;
 	private String email;
+	private String aadharNum;
+	
+	public String getfName() {
+		return fName;
+	}
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+	public String getlName() {
+		return lName;
+	}
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFevPetName() {
+		return fevPetName;
+	}
+	public void setFevPetName(String fevPetName) {
+		this.fevPetName = fevPetName;
+	}
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+	public int getLogingStatus() {
+		return logingStatus;
+	}
+	public void setLogingStatus(int logingStatus) {
+		this.logingStatus = logingStatus;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAadharNum() {
+		return aadharNum;
+	}
+	public void setAadharNum(String aadharNum) {
+		this.aadharNum = aadharNum;
+	}
+	
 }
